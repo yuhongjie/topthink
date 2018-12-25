42259b4b00d52f95c5f496403300b68b5dfb5a79
winer <yuhongjie@live.cn>
2018-12-25 18:06:13 +0800

    commint to github

--- application/index/view/_layout/default.blade.php
+++ application/index/view/_layout/default.blade.php
@@ -2,7 +2,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>公共视图</title>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	<!-- not no -->
+	<meta name='renderer' content='webkit'>
+	<meta http-equiv='X-UA-Compatible' content='ie=edge'>
+	<meta name='description' content=''>
+	<meta name='keywords' content=''>
+	<title>@yield('title')--ThinkPHP 入门教程</title>
+	<link rel="stylesheet" href="">
 </head>
 <body>
 	<h1>{block name="content"}正文{/block}</h1>
