027165f2c65c32152b5de2546e1f9e3d63bdbc28
yuhongjie <yuhongjie@live.cn>
2018-12-25 22:34:08 +0800

    添加js内容

--- application/index/view/_layout/default.blade.php
+++ application/index/view/_layout/default.blade.php
@@ -45,6 +45,8 @@
 	<div class="container">
 		@yield('content')
 	</div>
-	
+	<script src="/static/jquery/jquery.slim.min.js"></script>
+	<script src="/static/jquery/popper.min.js"></script>
+	<script src="/static/js/app.js"></script>
 </body>
 </html>
