562ff71da0dfbb73df03c2ccad6f644a543a047c
winer <yuhongjie@live.cn>
2018-12-26 10:04:17 +0800

    删除旧的视图内容

--- application/user/view/_layout/default.blade.php
+++
@@ -1,5 +0,0 @@
-@include('_layout.header')
-<div class="container">
-	@yield('content')
-</div>
-@include('_layout.footer')
--- application/user/view/_layout/footer.blade.php
+++
@@ -1,5 +0,0 @@
-	<script src="/static/jquery/jquery.slim.min.js"></script>
-	<script src="/static/jquery/popper.min.js"></script>
-	<script src="/static/js/app.js"></script>
-</body>
-</html>
